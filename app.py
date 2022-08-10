e481c4a290117f41a9deac4023ff6ec4b135fad8
Tyler Nguyen <109756055+tylerdnguyen@users.noreply.github.com>
2022-08-10 07:16:39 +0000

    add dynamic pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_job_from_db
+from database import load_job_from_db, load_a_job_from_db
 
 app = Flask(__name__)
 
@@ -7,12 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     jobs= load_job_from_db()
-    return render_template('home.html', jobs=jobs, company_name='Jovian')
+    return render_template('home.html', jobs=jobs)
 
 @app.route("/jobs")
 def list_jobs():
     jobs= load_job_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_a_job_from_db(id)
+  if not job:
+    return "Not Found",404
+  return render_template('jobpage.html',job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -21,3 +21,12 @@ def load_job_from_db():
     for row in result.all():
       jobs.append(dict(row))
     return jobs
+
+def load_a_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text('select * from jobs where id = :val'),val=id)
+    rows  = result.all()
+    if len(rows)==0:
+      return None
+    else:
+      return dict(rows[0])
